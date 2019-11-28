671772e1a6edcae1c063d34a33cc9a9d009e8b8a
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-11-27 22:07:28 -0800

    Update initial state for loaded game

--- Skill.js
+++ Skill.js
@@ -1,12 +1,17 @@
 class Skill {
     constructor(level, cost, multiplier) {
-        this.level = level;
+        this.level = 0;
         this.cost = cost;
         this.multiplier = multiplier;
+        this.ajustCost(level);
     }
+
     levelUp() {
         ++this.level;
         this.cost = Math.ceil(this.cost * this.multiplier);
-        return null;
     }
 }
+
+Skill.prototype.ajustCost = function (level) {
+    for(let i = 0; i < level; ++i) this.levelUp();
+};
